e412f547ca031ac9d847653dfa3d9c0d6cf592df
Chris Eads <chriseads@gmail.com>
2019-09-11 14:13:28 -0400

    re-add points_to, start Node init

--- Algorithms_2_HW1.py
+++ Algorithms_2_HW1.py
@@ -4,13 +4,16 @@ file = sys.argv[1]
 def alg(file):
     '''Finds strongly connected components (SCCs) of a directed acyclic graph (DAG)'''
     class Node:
-        def __init__(self, name, fin_time=0, explored=False, leader=0):
+        def __init__(self, name, points_to, fin_time=0, explored=False, leader=0):
             self.name = name
+
+    G = {}
 
     with open(file) as f:
         for line in f:
             temp = line.rstrip('\n').split(' ')
-            print(temp) 
+            if temp[0] not in G:
+                G[temp[0]] = Node(temp[1]) 
 
 if __name__ == '__main__':
     alg(file)
