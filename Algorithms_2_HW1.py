19360341b1e08c1735283c72133ac8dde97a7244
Chris Eads <chriseads@gmail.com>
2019-09-11 13:04:22 -0400

    add points_to

--- Algorithms_2_HW1.py
+++ Algorithms_2_HW1.py
@@ -1,5 +1,5 @@
 def alg(file):
     '''Finds strongly connected components (SCCs) of a directed acyclic graph (DAG)'''
     class Node:
-        def __init__(self, name, fin_time=0, explored=False, leader=0):
+        def __init__(self, name, points_to=0, fin_time=0, explored=False, leader=0):
             self.name = name
