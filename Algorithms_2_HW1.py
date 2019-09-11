6d40a9626ce5acab8e7bab5cef830e530097ec27
Chris Eads <chriseads@gmail.com>
2019-09-11 16:07:53 -0400

    fix graph dict creation

--- Algorithms_2_HW1.py
+++ Algorithms_2_HW1.py
@@ -2,18 +2,27 @@ import sys
 
 file = sys.argv[1] 
 def alg(file):
-    '''Finds strongly connected components (SCCs) of a directed acyclic graph (DAG)'''
+    '''Finds strongly connected components (SCCs) of a graph''' 
     class Node:
         def __init__(self, name, points_to, fin_time=0, explored=False, leader=0):
             self.name = name
+            self.points_to = points_to
 
+    # create dict of all nodes
     G = {}
 
     with open(file) as f:
         for line in f:
             temp = line.rstrip('\n').split(' ')
+            print(temp[0], temp[1])
             if temp[0] not in G:
-                G[temp[0]] = Node(temp[1]) 
+                G[temp[0]] = Node(temp[0], points_to=[temp[1]]) 
+                if temp[1] not in G:
+                    G[temp[1]] = Node(temp[1], [])  
+            elif temp[0] in G:
+                G[temp[0]].points_to.append(temp[1])
+    for i in G:
+        print(i, G[i].points_to)
 
 if __name__ == '__main__':
     alg(file)
