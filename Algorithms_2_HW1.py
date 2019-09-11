155b06f4bb70d527795ed5f4a63fc44a50e00b6a
Chris Eads <chriseads@gmail.com>
2019-09-11 13:49:00 -0400

    add file handling

--- Algorithms_2_HW1.py
+++ Algorithms_2_HW1.py
@@ -1,10 +1,16 @@
+import sys
+
+file = sys.argv[1] 
 def alg(file):
     '''Finds strongly connected components (SCCs) of a directed acyclic graph (DAG)'''
     class Node:
         def __init__(self, name, fin_time=0, explored=False, leader=0):
             self.name = name
 
-    
+    with open(file) as f:
+        for line in f:
+            temp = line.rstrip('\n').split(' ')
+            print(temp) 
 
 if __name__ == '__main__':
     alg(file)
